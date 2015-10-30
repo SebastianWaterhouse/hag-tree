154fb5539a924e25e5c6279063398b1897d7be26
Sebastian W <littlestllama6@gmail.com>
2015-10-30 12:50:17 -0500

    Actually made mood.py, fixed mood code.
    Moods now use instances instead of full-on classes.
    
    Breaks game even more.

--- hag-tree-game/mood.py
+++ hag-tree-game/mood.py
@@ -1,1 +1,13 @@
-import hagtreemain as hag
+class moods(object):
+  init(req, name, col):
+    self.req=req
+    self.name=name
+    self.color=col
+
+#base moods#
+
+fine = moods(0, 'Fine', '07')
+
+angry = moods(-10, 'Angry', '0c')
+
+happy = moods(10, 'Happy', '0a')
